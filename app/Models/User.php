5d8b1178bb7244ec5aa05ed45467eeff76d0863e
leonardoserra <serra.leonardo92@gmail.com>
2023-11-15 18:51:34 +0100

    created user migration with all the columns

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'description',
+        'profile_picture_path',
+        'visibility'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,14 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    //Chefs table
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 150)->required()->unique();
+            $table->string('email', 200)->required()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',150)->required();
+            $table->text('description',150)->nullable();
+            $table->string('profile_picture_path')->default("/img/default_img.png");
+            $table->tinyInteger('visibility')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
