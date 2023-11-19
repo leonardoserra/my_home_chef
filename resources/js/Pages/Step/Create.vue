93f0ace5d9e9eb115e3347842320103d39d226c3
leonardoserra <serra.leonardo92@gmail.com>
2023-11-19 01:06:12 +0100

    render info recipe

--- resources/js/Pages/Step/Create.vue
+++ resources/js/Pages/Step/Create.vue
@@ -17,11 +17,16 @@ const form = useForm({
 <template>
     <Head title="Passaggi della Ricetta" />
     <AuthenticatedLayout>
-        {{ recipe }}
-        <div class="recipe-info">
-            
+        <!-- {{ recipe }} -->
+        <div class="m-auto recipe-info">
+            <h1 class="">{{ recipe.name }}</h1>
+            <h1 class="">{{ recipe.description == " "?'Nessuna descrizione': recipe.description}}</h1>
+            <h1 class="">{{ recipe.duration }}</h1>
+            <h1 class="">{{ recipe.recipe_points }}</h1>
+            <h1 class="">{{ recipe.difficulty_id??="Difficoltà non selezionata" }}</h1>
+            <h1 class="">{{ recipe.meal_type_id??="Tipo di pietanza non selezionato" }}</h1>
+            <h1 class="">{{ recipe.cuisine_id??="Cucina non selezionata" }}</h1>
         </div>
-        <h1 class="">{{ recipe.name }}</h1>
     </AuthenticatedLayout>
 </template>
 
