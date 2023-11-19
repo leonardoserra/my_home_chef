b644883d1c5d232d46b64efaef22d7a4aba04614
leonardoserra <serra.leonardo92@gmail.com>
2023-11-19 01:07:50 +0100

    render recipe info

--- resources/js/Pages/Step/Create.vue
+++ resources/js/Pages/Step/Create.vue
@@ -19,13 +19,13 @@ const form = useForm({
     <AuthenticatedLayout>
         <!-- {{ recipe }} -->
         <div class="m-auto recipe-info">
-            <h1 class="">{{ recipe.name }}</h1>
-            <h1 class="">{{ recipe.description == " "?'Nessuna descrizione': recipe.description}}</h1>
-            <h1 class="">{{ recipe.duration }}</h1>
-            <h1 class="">{{ recipe.recipe_points }}</h1>
-            <h1 class="">{{ recipe.difficulty_id??="Difficoltà non selezionata" }}</h1>
-            <h1 class="">{{ recipe.meal_type_id??="Tipo di pietanza non selezionato" }}</h1>
-            <h1 class="">{{ recipe.cuisine_id??="Cucina non selezionata" }}</h1>
+            <h1 class="">Nome: {{ recipe.name }}</h1>
+            <h1 class="">Descrizione: {{ recipe.description == " "?'Nessuna descrizione': recipe.description}}</h1>
+            <h1 class="">Durata Preparazione: {{ recipe.duration }}</h1>
+            <h1 class="">Punti Ricetta: {{ recipe.recipe_points }}</h1>
+            <h1 class="">Difficoltà: {{ recipe.difficulty_id??="Difficoltà non selezionata" }}</h1>
+            <h1 class="">Tipo Pietanza: {{ recipe.meal_type_id??="Tipo di pietanza non selezionato" }}</h1>
+            <h1 class="">Tipo di Cucina: {{ recipe.cuisine_id??="Cucina non selezionata" }}</h1>
         </div>
     </AuthenticatedLayout>
 </template>
