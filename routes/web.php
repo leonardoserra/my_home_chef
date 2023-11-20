9ed5074859ef9af57cb96aca651b58679adb9c07
leonardoserra <serra.leonardo92@gmail.com>
2023-11-20 16:06:51 +0100

    recipe routes with id dependency injected

--- routes/web.php
+++ routes/web.php
@@ -32,20 +32,29 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('/recipe', RecipeController::class)
-    ->middleware(['auth'])->parameters([
-        'recipe'=>'recipe:id',
-    ]);
+// Route::resource('/recipe', RecipeController::class)
+//     ->middleware(['auth']);
     
+Route::middleware('auth')->group(function () {
+    Route::get('/recipe', [RecipeController::class, 'index'])->name('recipe.index');
+    Route::get('/recipe/create', [RecipeController::class, 'create'])->name('recipe.create');
+    Route::post('/recipe', [RecipeController::class, 'store'])->name('recipe.store');
+    Route::get('/recipe/{id}', [RecipeController::class, 'show'])->name('recipe.show');
+    Route::get('/recipe/{id}/edit', [RecipeController::class, 'edit'])->name('recipe.edit');
+    Route::patch('/recipe/{id}', [RecipeController::class, 'update'])->name('recipe.update');
+    Route::delete('/recipe/{id}', [RecipeController::class, 'destroy'])->name('recipe.destroy');
+});
 Route::resource('/step', StepController::class)
     ->middleware(['auth']);
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+Route::middleware('auth')->group(function () {
+    
     Route::get('/difficulties', [DifficultyController::class, 'index'])->name('difficulty.show');
 });
 
